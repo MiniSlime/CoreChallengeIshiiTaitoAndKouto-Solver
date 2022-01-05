2da714982b5e68af48500fe829638f7dd5994d63
FatherScorpion <FatherScorpion@outlook.jp>
2022-01-05 19:18:54 +0900

    ran

--- solver.py
+++ solver.py
@@ -1,13 +1,16 @@
-import networkx as nx
-import matplotlib.pyplot as plt
 import random
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
 graph = nx.Graph() #無向グラフを作成 
 a=0
 b=0
-for i in range(100):
-  b=random.randint(0,99)
+ran=100
+for i in range(ran):
+  b=random.randint(0,ran-1)
   while a==b:
-    b=random.randint(0,99)
+    b=random.randint(0,ran-1)
   graph.add_edge(a,b)
   a=b
 
